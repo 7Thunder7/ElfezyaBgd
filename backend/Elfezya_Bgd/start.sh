e422b9ece33d65edbabf0f2d9dc711f5ad3616e9
7Thunder7 <stevenmaged2018@gmail.com>
2026-03-20 13:34:24 +0200

    editing start.sh to accept uername, email, password.

--- backend/Elfezya_Bgd/start.sh
+++ backend/Elfezya_Bgd/start.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python manage.py migrate --noinput
-gunicorn Elfezya_Bgd.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers ${WEB_CONCURRENCY:-2}
+python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+from os import getenv
+
+User = get_user_model()
+
+username = getenv("DJANGO_SUPERUSER_USERNAME")
+email = getenv("DJANGO_SUPERUSER_EMAIL")
+password = getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superuser created.")
+    else:
+        print("Superuser already exists.")
+else:
+    print("Superuser env vars not set. Skipping.")
+EOF
+
+gunicorn Elfezya_Bgd.wsgi:application
